0c45fc26e8f84b43e2d4e338793ea318df29ade7
Erik Dubois <erik.dubois@gmail.com>
2017-03-02 22:24:00 +0100

    Comment :  on Thu Mar  2 22:24:00 CET 2017

---
+++ installation/430-install-software-archlabs-v1.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+sudo pacman -S audacious --needed --noconfirm
+sudo pacman -S xfce4-appfinder --needed --noconfirm
+sudo pacman -S xorg-xkill --needed --noconfirm
+sudo pacman -S viewnior --needed --noconfirm
+sudo pacman -S audacity --needed --noconfirm
+sudo pacman -S xfburn --needed --noconfirm
+sudo pacman -S gtk-recordmydesktop --needed --noconfirm
+sudo pacman -S opera --needed --noconfirm
+sudo pacman -S libreoffice --needed --noconfirm
+sudo pacman -S gnumeric --needed --noconfirm
+
+
+package="pasystray-gtk3-standalone"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+
+fi
+
+
+package="gigolo"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+
+fi
+
+
+echo "################################################################"
+echo "#########      pasystray software installed     ################"
+echo "################################################################"
