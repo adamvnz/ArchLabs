4dee44f65137173d6c4330e62afb307c4374aeae
Erik Dubois <erik.dubois@gmail.com>
2017-03-02 17:21:15 +0100

    Comment :  on Thu Mar  2 17:21:15 CET 2017

--- installation/410-install-software-distro-specific-v1.sh
+++ installation/410-install-software-distro-specific-v1.sh
@@ -17,13 +17,55 @@ echo "################################################################"
 
 
 sudo pacman -S obmenu --needed --noconfirm
-sudo pacman -S obkey --needed --noconfirm
+
+# only in aur
+#sudo pacman -S obkey --needed --noconfirm
 sudo pacman -S notify-osd --needed --noconfirm
 sudo pacman -S oblogout --needed --noconfirm
 sudo pacman -S obconf --needed --noconfirm
-sudo pacman -S orage --needed --noconfirm
+#sudo pacman -S orage --needed --noconfirm
 sudo pacman -S gsimplecal --needed --noconfirm
 sudo pacman -S numlockx --needed --noconfirm
+
+echo "################################################################"
+echo "obkey-git"   
+echo "################################################################"
+
+
+package="obkey-git"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+
+fi
+
 
 
 echo "################################################################"
