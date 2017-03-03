1fd27101c592c61bc86ed7e866e2940e758e8e69
Erik Dubois <erik.dubois@gmail.com>
2017-03-03 17:30:40 +0100

    Comment :  on Fri Mar  3 17:30:40 CET 2017

---
+++ installation/personal/725-install-personal-settings-remove-virtual-box-message-v1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+echo "remove the message about virtual box when you restart"
+echo "if you are not on virtualbox you can run this"
+
+
+sudo rm /etc/xdg/autostart/vboxclient.desktop
+
+
+
+echo "################################################################"
+echo "#########    virtual box should be gone at next boot    ########"
+echo "################################################################"
+
