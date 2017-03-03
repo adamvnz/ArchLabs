4225b995b315b93ffcab2e9e3f7295d16aa0c418
Erik Dubois <erik.dubois@gmail.com>
2017-03-03 07:47:05 +0100

    Comment :  on Fri Mar  3 07:47:05 CET 2017

--- installation/430-install-software-archlabs-v1.sh
+++ installation/430-install-software-archlabs-v1.sh
@@ -93,6 +93,43 @@ else
 fi
 
 
+
+package="xfce-slimlock"
+
+#----------------------------------------------------------------------------------
+
+#checking if application is already installed or else install with aur helpers
+if pacman -Qi $package &> /dev/null; then
+
+	echo "################################################################"
+	echo "################## "$package" is already installed"
+	echo "################################################################"
+
+else
+
+	#checking which helper is installed
+	if pacman -Qi packer &> /dev/null; then
+
+		echo "Installing with packer"
+		packer -S --noconfirm --noedit  $package
+
+	elif pacman -Qi pacaur &> /dev/null; then
+		
+		echo "Installing with pacaur"
+		pacaur -S --noconfirm --noedit  $package
+		 	
+	elif pacman -Qi yaourt &> /dev/null; then
+
+		echo "Installing with yaourt"
+		yaourt -S --noconfirm $package
+			  	
+	fi
+
+
+fi
+
+
+
 echo "################################################################"
 echo "#########      pasystray software installed     ################"
 echo "################################################################"
