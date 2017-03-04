916c363cef9c1a1c4f4076633b36a43983cacc10
Erik Dubois <erik.dubois@gmail.com>
2017-03-04 21:21:00 +0100

    Comment :  on Sat Mar  4 21:21:00 CET 2017

--- installation/personal/745-install-personal-settings-remove-virtual-box-message-v2.sh
+++ installation/personal/745-install-personal-settings-remove-virtual-box-message-v2.sh
@@ -17,10 +17,13 @@ echo "if you are not on virtualbox you can run this"
 
 [ -f /etc/xdg/autostart/vboxclient.desktop ] && sudo rm /etc/xdg/autostart/vboxclient.desktop
 
+# remove as well software installed for virtualbox support once you are on ssd/harddisk
+# http://archlabs.prophpbb.com/topic21.html
 
-
+sudo pacman -Rns virtualbox-guest-utils virtualbox-guest-modules-arch
+sudo rm /etc/modules-load.d/virtualbox.conf
 
 echo "################################################################"
-echo "#########    virtual box should be gone at next boot    ########"
+echo "#########    virtual box for guest has been remoced     ########"
 echo "################################################################"
 
