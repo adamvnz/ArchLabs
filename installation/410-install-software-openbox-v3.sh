346eb75254efe69c2bb54a67064e01eb2c08d2ac
Erik Dubois <erik.dubois@gmail.com>
2017-03-05 08:31:41 +0100

    Comment :  on Sun Mar  5 08:31:41 CET 2017

--- installation/410-install-software-openbox-v3.sh
+++ installation/410-install-software-openbox-v3.sh
@@ -1028,59 +1028,5 @@ fi
 
 
 echo "################################################################"
-echo "vivaldi"
-echo "################################################################"
-
-
-package="vivaldi"
-command="vivaldi-stable"
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-	# Just checking if installation was successful
-	if pacman -Qi $package &> /dev/null; then
-	
-	echo "################################################################"
-	echo "#########  "$package" has been installed"
-	echo "################################################################"
-
-	else
-
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-	echo "!!!!!!!!!  "$package" has NOT been installed"
-	echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-	fi
-
-fi
-
-
-echo "################################################################"
 echo "#########   distro specific software installed  ################"
 echo "################################################################"
